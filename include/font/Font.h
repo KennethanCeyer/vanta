aac160bd4c96dbea8a597c8267f2f25a00a8f06b
Kenneth Ceyer <kennethan@nhpcw.com>
2018-10-26 17:52:41 +0900

    add default file structures

---
+++ include/font/Font.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018 PIGNOSE. All rights reserved.
+ *
+ * This source is govered under the MIT license.
+ * Read README and LICENSE file of this project for more information.
+ *
+ */
+
+#ifndef PROJECT_FONT_H
+#define PROJECT_FONT_H
+
+#endif //PROJECT_FONT_H
---
+++ src/font/Font.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2018 PIGNOSE. All rights reserved.
+ *
+ * This source is govered under the MIT license.
+ * Read README and LICENSE file of this project for more information.
+ *
+ */
+
+#include "../../include/font/Font.h"
